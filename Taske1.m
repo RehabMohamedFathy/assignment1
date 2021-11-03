a8687dea8baf0e89a29e7d6e13580c687910ddcd
RehabMohamedFathy <93394913+RehabMohamedFathy@users.noreply.github.com>
2021-11-03 19:30:39 +0200

    Add files via upload

---
+++ Taske1.m
@@ -0,0 +1,74 @@
+A=[-7 5 -9 ;2 -1 2;1 -1 2];
+B=[16 3 2 13;5 10 11 8;9 6 7 12;4 15 14 1];
+C=[4 2 -3 ;7 -7 9;3 -5 6];
+D=[6 3 2 ;2 12 -7;-1 6 2 ;-5 15 11];
+
+%Question one 
+X=(3*A)-(5*C);
+
+%%
+%Question Two
+Y=(7*A)+(2*B);
+
+%%
+%Question Three
+Z=C*A;
+
+%%
+%Question Four
+M=C*D;
+
+%%
+%Question Five
+N=zeros(5);
+
+%%
+%Question Six
+o=zeros(5,4);
+
+%%
+%Question seven
+p=ones(4);
+
+%%
+%Question Eight
+Q=ones(5,4);
+
+%%
+%Question Nine
+R=size(D);
+
+%%
+%Question Ten
+S=zeros(size(D));
+
+%%
+%Question eleven
+T=diag([1 2 3 4]);
+
+%%
+%Question Twelve
+U=eye(4);
+
+%%
+%Question Thirteen
+V=[A,B];
+
+%%
+%Question fourteen
+W=(A;B);
+
+%%
+%Question Fifteen
+H1=diag([5 5 5 5 5 5 5]);
+H2=[5;5;5;5;5;5;5];
+H=[H1,H2];
+
+%%
+%Question Sixteen
+K=A(4,:);
+
+%%
+%Question Seventeen
+l=A(:,4);
+%%
